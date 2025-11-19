895a664c5c7303766c5a6b2a05460ebbbe7a7ef9
Sudheer <146576150+CleverSudheer@users.noreply.github.com>
2025-11-19 20:08:05 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ca1
+
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText( this,  "Log Out Sucessfully", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
